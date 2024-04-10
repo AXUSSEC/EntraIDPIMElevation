e6174bc1da8232fdf26b25c3bc2149ab8b5c7a7c
RyanAndrwMoore <ryan.moore@anticimex.com>
2024-04-10 10:03:03 -0700

    Formatting

--- invoke-EntraIDPimElevations.ps1
+++ invoke-EntraIDPimElevations.ps1
@@ -67,34 +67,34 @@ Script now supports the following features:
 #Requires -Module Microsoft.Graph.Groups
 
 param (
-    [Parameter(Mandatory=$true, ParameterSetName='ActivateRoles', HelpMessage="Specifies the roles to activate.")]
+    [Parameter(Mandatory = $true, ParameterSetName = 'ActivateRoles', HelpMessage = 'Specifies the roles to activate.')]
     [ValidateNotNullOrEmpty()]
     [string]$RolesToActivate,
-    [Parameter(Mandatory=$true, ParameterSetName='ActivateRoles', HelpMessage="Specifies justification for activation.")]
-    [Parameter(Mandatory=$true, ParameterSetName='ActivateGroups', HelpMessage="Specifies justification for activation.")]
+    [Parameter(Mandatory = $true, ParameterSetName = 'ActivateRoles', HelpMessage = 'Specifies justification for activation.')]
+    [Parameter(Mandatory = $true, ParameterSetName = 'ActivateGroups', HelpMessage = 'Specifies justification for activation.')]
     [ValidateNotNullOrEmpty()]
     [ValidateLength(6, 200)]
     [string]$Justification,
-    [Parameter(Mandatory=$false, ParameterSetName='ActivateRoles', HelpMessage="Get all available eligible roles for the user.")]
-    [Parameter(Mandatory=$false, ParameterSetName='ActivateGroups', HelpMessage="Specifies justification for activation.")]
-    [Parameter(Mandatory=$false, ParameterSetName='GetRoles', HelpMessage="Get all available eligible roles for the user.")]
-    [Parameter(Mandatory=$false, ParameterSetName='GetGroups', HelpMessage="Get all available eligible groups for the user.")]
+    [Parameter(Mandatory = $false, ParameterSetName = 'ActivateRoles', HelpMessage = 'Get all available eligible roles for the user.')]
+    [Parameter(Mandatory = $false, ParameterSetName = 'ActivateGroups', HelpMessage = 'Specifies justification for activation.')]
+    [Parameter(Mandatory = $false, ParameterSetName = 'GetRoles', HelpMessage = 'Get all available eligible roles for the user.')]
+    [Parameter(Mandatory = $false, ParameterSetName = 'GetGroups', HelpMessage = 'Get all available eligible groups for the user.')]
     [switch]$ForceRefresh,
-    [Parameter(Mandatory=$false, ParameterSetName='ActivateRoles', HelpMessage="Specifies the duration of the activation.")]
-    [Parameter(Mandatory=$false, ParameterSetName='ActivateGroups', HelpMessage="Specifies justification for activation.")]
+    [Parameter(Mandatory = $false, ParameterSetName = 'ActivateRoles', HelpMessage = 'Specifies the duration of the activation.')]
+    [Parameter(Mandatory = $false, ParameterSetName = 'ActivateGroups', HelpMessage = 'Specifies justification for activation.')]
     [int]$ActivationDuration = 8,
-    [Parameter(Mandatory=$true, ParameterSetName='ActivateGroups', HelpMessage="Specifies the groups to activate.")]
+    [Parameter(Mandatory = $true, ParameterSetName = 'ActivateGroups', HelpMessage = 'Specifies the groups to activate.')]
     [ValidateNotNullOrEmpty()]
     [string]$GroupsToActivate,
-    [Parameter(Mandatory=$true, ParameterSetName='ActivateGroups', HelpMessage="Specifies owner or member for activation.")]
+    [Parameter(Mandatory = $true, ParameterSetName = 'ActivateGroups', HelpMessage = 'Specifies owner or member for activation.')]
     [ValidateNotNullOrEmpty()]
-    [ValidateSet("Owner", "Member")]
+    [ValidateSet('Owner', 'Member')]
     [string]$AccessType,
-    [Parameter(Mandatory=$false, ParameterSetName='GetRoles', HelpMessage="Get all available eligible roles for the user.")]
+    [Parameter(Mandatory = $false, ParameterSetName = 'GetRoles', HelpMessage = 'Get all available eligible roles for the user.')]
     [switch]$GetAvailableRoles,
-    [Parameter(Mandatory=$false, ParameterSetName='GetGroups', HelpMessage="Get all available eligible groups for the user.")]
+    [Parameter(Mandatory = $false, ParameterSetName = 'GetGroups', HelpMessage = 'Get all available eligible groups for the user.')]
     [switch]$GetAvailableGroups,
-    [Parameter(Mandatory=$true, ParameterSetName='Consent', HelpMessage="Request consent on scopes.")]
+    [Parameter(Mandatory = $true, ParameterSetName = 'Consent', HelpMessage = 'Request consent on scopes.')]
     [switch]$Consent
 )
 # Clear out any existing tokens 
@@ -117,10 +117,9 @@ if ($Consent) {
         Connect-MgGraph -Scopes $ScopesString -NoWelcome -ErrorAction Stop
         Write-Host 'Consent OK, exiting script' -ForegroundColor Green
         Disconnect-MgGraph -ErrorAction SilentlyContinue | Out-Null
-    exit      
-    }
-    catch {
-        Write-Host "Consent not granted, admin request might be needed" -ForegroundColor Yellow
+        exit      
+    } catch {
+        Write-Host 'Consent not granted, admin request might be needed' -ForegroundColor Yellow
         Disconnect-MgGraph -ErrorAction SilentlyContinue | Out-Null
         exit
     }
@@ -139,73 +138,70 @@ try {
     }#>
     $host.ui.RawUI.WindowTitle = $($MyContext.Account)
     Write-Output "Connected as $($MyContext.Account)"     
-}
-catch {
+} catch {
     Write-Warning "Failed to connect to Graph: $($_.Exception.Message), exiting script"
     Disconnect-MgGraph -ErrorAction SilentlyContinue | Out-Null
     exit
 }
 
 switch ($PSCmdlet.ParameterSetName) {
-    "GetGroups"{
+    'GetGroups' {
         $MyEligibleGroups = Get-MgIdentityGovernancePrivilegedAccessGroupEligibilityScheduleInstance -Filter "principalId eq '$Me'" | Select-Object accessId, GroupId
         if ($($MyEligibleGroups.count) -eq 0) {
-            Write-Warning "No eligible groups found, exiting script"
+            Write-Warning 'No eligible groups found, exiting script'
             break
         }
         Write-Output "You have $($MyEligibleGroups.count) eligible groups and they are:"
         foreach ($group in $MyEligibleGroups) {
-            $CurrentGroup = Get-MGGroup -GroupID $group.GroupId
+            $CurrentGroup = Get-MgGroup -GroupId $group.GroupId
             Write-Output "Type: $($group.accessId) : $($CurrentGroup.DisplayName)"
         }
         break
     }
-    "GetRoles"{
+    'GetRoles' {
         # Try to get all eligible roles for user for processing
         try {
             $myRoles = Get-MgRoleManagementDirectoryRoleEligibilitySchedule -ExpandProperty RoleDefinition -All -Filter "principalId eq '$Me'" -ErrorAction Stop
-        }
-        catch {
+        } catch {
             Write-Warning "Failed to get eligible roles: $($_.Exception.Message), exiting script"
             Disconnect-MgGraph -ErrorAction SilentlyContinue | Out-Null
             exit
         }
         if ($($myroles.count) -eq 0) {
-            Write-Warning "No eligible roles found, exiting script"
+            Write-Warning 'No eligible roles found, exiting script'
             break
         }
         Write-Output "You have $($myroles.count) eligible roles and they are:"
         Write-Output $($myRoles.RoleDefinition.DisplayName | Sort-Object | Format-Table)
         break
     }
-    "ActivateRoles"{
+    'ActivateRoles' {
         try {
             $myRoles = Get-MgRoleManagementDirectoryRoleEligibilitySchedule -ExpandProperty RoleDefinition -All -Filter "principalId eq '$Me'" -ErrorAction Stop
-        }
-        catch {
+        } catch {
             Write-Warning "Failed to get eligible roles: $($_.Exception.Message), exiting script"
             Disconnect-MgGraph -ErrorAction SilentlyContinue | Out-Null
             exit
         }
         if ($($myroles.count) -eq 0) {
-            Write-Warning "No eligible roles found, exiting script"
+            Write-Warning 'No eligible roles found, exiting script'
             break
         }
         $MyRolesNames = $myRoles.RoleDefinition.DisplayName
-        $Roles = $RolesToActivate.Split(",") | ForEach-Object { $_.Trim() }
+        $Roles = $RolesToActivate.Split(',') | ForEach-Object { $_.Trim() }
         foreach ($role in $Roles) {
             if ($role -notin $MyRolesNames) {
                 Write-Warning "Role $role is not eligible for you to activate, exiting script"
                 exit
             }
         }
-        Write-Host "You are about to activate the following roles:" -ForegroundColor Green
+        Write-Host 'You are about to activate the following roles:' -ForegroundColor Green
         Write-Output $Roles | Sort-Object | Format-Table
         Write-Output "This will be active for $($ActivationDuration) hours"
-        Write-Host "Are you sure you want to continue? (Y/N)" -ForegroundColor Yellow
+        Write-Host 'Are you sure you want to continue? (Y/N)' -ForegroundColor Yellow
         $answer = Read-Host
-        if ($answer -ne "Y") {
-            Write-Output "Exiting script"
+        if ($answer -ne 'Y') {
+            Write-Output 'Exiting script'
             break
         }
         Write-Output "Activating roles: $RolesToActivate"
@@ -213,18 +209,18 @@ switch ($PSCmdlet.ParameterSetName) {
         
         # Activate the roles
         foreach ($role in $Roles) {
-            $myRole = $myroles | Where-Object {$_.RoleDefinition.DisplayName -eq $role}
+            $myRole = $myroles | Where-Object { $_.RoleDefinition.DisplayName -eq $role }
             #Write-Output $myRole
             $params = @{
-                Action = "selfActivate"
-                PrincipalId = $myRole.PrincipalId
+                Action           = 'selfActivate'
+                PrincipalId      = $myRole.PrincipalId
                 RoleDefinitionId = $myRole.RoleDefinitionId
                 DirectoryScopeId = $myRole.DirectoryScopeId
-                Justification = "Enable $role role with reason $Justification"
-                ScheduleInfo = @{
+                Justification    = "Enable $role role with reason $Justification"
+                ScheduleInfo     = @{
                     StartDateTime = Get-Date
-                    Expiration = @{
-                        Type = "AfterDuration"
+                    Expiration    = @{
+                        Type     = 'AfterDuration'
                         Duration = "PT$($ActivationDuration)H"
                     }
                 }
@@ -232,68 +228,66 @@ switch ($PSCmdlet.ParameterSetName) {
             try {
                 $Activation = New-MgRoleManagementDirectoryRoleAssignmentScheduleRequest -BodyParameter $params -ErrorAction Stop
                 Write-Output "Activated role $($role) for $($ActivationDuration) hours from UTC $($Activation.ScheduleInfo.StartDateTime) and will expire at UTC $($Activation.ScheduleInfo.StartDateTime.AddHours($ActivationDuration))" 
-            }
-            catch {
+            } catch {
                 <#Do this if a terminating exception happens#>
                 Write-Warning "Error activating role $($role): $($_.Exception.Message)"
                 
             }
         }
     }
-    "ActivateGroups"{
+    'ActivateGroups' {
         try {
-            $MyEligibleGroups = Get-MgIdentityGovernancePrivilegedAccessGroupEligibilityScheduleInstance -Filter "principalId eq '$Me'" | Select-Object accessId, GroupId | Where-Object AccessId -eq $AccessType -ErrorAction Stop
+            $MyEligibleGroups = Get-MgIdentityGovernancePrivilegedAccessGroupEligibilityScheduleInstance -Filter "principalId eq '$Me'" | Select-Object accessId, GroupId | Where-Object AccessId -EQ $AccessType -ErrorAction Stop
 
-        }
-        catch {
+        } catch {
             Write-Warning "Failed to get eligible groups: $($_.Exception.Message), exiting script"
             Disconnect-MgGraph -ErrorAction SilentlyContinue | Out-Null
             exit
         }
         if ($($MyEligibleGroups.count) -eq 0) {
-            Write-Warning "No eligible groups found, exiting script"
+            Write-Warning 'No eligible groups found, exiting script'
             break
         }
         $EligibleGroups = @()
         foreach ($group in $MyEligibleGroups) {
-            $CurrentGroup = Get-MGGroup -GroupID $group.GroupId
+            $CurrentGroup = Get-MgGroup -GroupId $group.GroupId
             $EligibleGroups += [PSCustomObject]@{
-                accessId = $group.accessId
-                GroupId = $group.GroupId
+                accessId    = $group.accessId
+                GroupId     = $group.GroupId
                 DisplayName = $CurrentGroup.DisplayName
             }
             #Write-Output "Type: $($group.accessId) : $($CurrentGroup.DisplayName)"
         }
-        $Groups = $GroupsToActivate.Split(",") | ForEach-Object { $_.Trim() }
+        $Groups = $GroupsToActivate.Split(',') | ForEach-Object { $_.Trim() }
         foreach ($group in $Groups) {
             if ($group -notin $EligibleGroups.DisplayName) {
                 Write-Warning "Group $group is not eligible for you to activate, exiting script"
                 exit
             }
         }
-        Write-Host "You are about to activate the following groups:" -ForegroundColor Green
+        Write-Host 'You are about to activate the following groups:' -ForegroundColor Green
         Write-Output $Groups | Sort-Object | Format-Table
         Write-Output "This will be active for $($ActivationDuration) hours"
-        Write-Host "Are you sure you want to continue? (Y/N)" -ForegroundColor Yellow
+        Write-Host 'Are you sure you want to continue? (Y/N)' -ForegroundColor Yellow
         $answer = Read-Host
-        if ($answer -ne "Y") {
-            Write-Output "Exiting script"
+        if ($answer -ne 'Y') {
+            Write-Output 'Exiting script'
             break
         }
         Write-Output "Activating groups: $GroupsToActivate"
         Write-Output "Reason: $Justification"
         foreach ($group in $Groups) {
-            $MyGroupIdToElevate = ($EligibleGroups | Where-Object {$_.DisplayName -eq $group}).GroupId
-            $accessId = ($EligibleGroups | Where-Object {$_.DisplayName -eq $group}).accessId
+            $MyGroupIdToElevate = ($EligibleGroups | Where-Object { $_.DisplayName -eq $group }).GroupId
+            $accessId = ($EligibleGroups | Where-Object { $_.DisplayName -eq $group }).accessId
             $params = @{
-                accessId = $accessId
-                principalId = $Me
-                groupId = $MyGroupIdToElevate
-                action = "selfActivate"
-                scheduleInfo = @{
+                accessId      = $accessId
+                principalId   = $Me
+                groupId       = $MyGroupIdToElevate
+                action        = 'selfActivate'
+                scheduleInfo  = @{
                     startDateTime = Get-Date
-                    expiration = @{
-                        type = "afterDuration"
+                    expiration    = @{
+                        type     = 'afterDuration'
                         duration = "PT$($ActivationDuration)H"
                     }
                 }
@@ -302,14 +296,13 @@ switch ($PSCmdlet.ParameterSetName) {
             try {
                 $MyActivation = New-MgIdentityGovernancePrivilegedAccessGroupAssignmentScheduleRequest -BodyParameter $params -ErrorAction Stop 	
                 Write-Output "Actived group $($group) $($accessId) for $($ActivationDuration) hours"
-            }
-            catch {
+            } catch {
                 <#Do this if a terminating exception happens#>
                 Write-Output "Failed activating $($accessId) to group $($group)"
                 Write-Warning "$($_.Exception.Message)"
             }
         }           
     }
-    Default {exit}
+    Default { exit }
 }
 #Disconnect-MgGraph -ErrorAction SilentlyContinue | Out-Null
